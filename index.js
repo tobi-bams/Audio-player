a5344b9cbaf1da207abbab7483c26e3f7c20a61a
tobi-bams <teebams49@gmail.com>
2020-11-28 23:20:09 +0100

    trying stuffs out

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const playButton = document.getElementById("playButton");
 let audio = document.createElement("audio");
+currentlyPlaying = 0;
 
 const musicLibrary = [{title: "Tonight", artist: "Nonso Amadi"}, {title: "Say Something", artist: "A Great Big World"},
                         {title: "No Longer Beneficial", artist: "Simi"}, {title: "Never Enough", artist: "Loren Allred"},
                         {title: "Someone Like You", artist: "Adele"}];
 
-audio.setAttribute("src", `assets/${musicLibrary[1].title.toLocaleLowerCase()}.mp3`);
+audio.setAttribute("src", `assets/${musicLibrary[currentlyPlaying].title.toLocaleLowerCase()}.mp3`);
 audio.play();
 playButton.addEventListener("click", (evt) => {
-    audio.pause();
+    audio.play();
+})
+
+audio.addEventListener("timeupdate", (evt) => {
+    if(audio.currentTime === audio.duration){
+        currentlyPlaying += 1;
+        if(currentlyPlaying < musicLibrary.length){
+            audio.setAttribute("src", `assets/${musicLibrary[currentlyPlaying].title.toLocaleLowerCase()}.mp3`);
+            audio.play();
+            console.log(audio.played)
+        }
+    }
 })
