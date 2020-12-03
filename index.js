1e33fcba48385eb2f8183bf2dc47df5dd009304c
tobi-bams <teebams49@gmail.com>
2020-12-03 16:33:17 +0100

    modifying the forward feature

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ let forward = document.getElementById("forward");
 let audioStatus = document.getElementById("audioStatus");
 let audioRange = document.getElementById("audioRange");
 let audioStatusContainer = "start";
+let playingStatus = document.getElementById("playingStatus");
 
 const musicLibrary = [{title: "Tonight", artist: "Nonso Amadi"}, {title: "Say Something", artist: "A Great Big World"},
                         {title: "No Longer Beneficial", artist: "Simi"}, {title: "Never Enough", artist: "Loren Allred"},
@@ -39,9 +40,24 @@ audio.addEventListener("timeupdate", (evt) => {
         }
         else{
             audioStatus.setAttribute("src", "assets/controls/play.svg");
+            audioStatusContainer = "playing";
         }
     }
+    let currentPlayingTime = duration (audio.currentTime);
+    let totalDuration = duration(audio.duration);
+    if(totalDuration == "NaN:NaN"){
+        totalDuration = "--:--";
+    }
+    playingStatus.textContent = `${currentPlayingTime} / ${totalDuration}`;
 })
+
+function duration(value){
+    let min = Math.floor(value/60);
+    let sec = Math.floor(value - (min * 60));
+    
+    let result = `${min}:${sec}`;
+    return result;
+}
 
 audioRange.addEventListener("input", () => {
     audio.currentTime = (audioRange.value * audio.duration) / 600;
@@ -51,9 +67,9 @@ forward.addEventListener("click", () => {
     currentlyPlaying += 1;
     if(currentlyPlaying < musicLibrary.length){
         audio.setAttribute("src", `assets/${musicLibrary[currentlyPlaying].title.toLocaleLowerCase()}.mp3`);
-        if(audioStatusContainer === "paused"){
+        audioStatus.setAttribute("src", "assets/controls/pause.svg")
             audio.play();
-        }
+            audioStatusContainer = "paused";
     }
     else{
         currentlyPlaying -= 1;
@@ -64,9 +80,9 @@ backward.addEventListener("click", () => {
     currentlyPlaying -= 1;
     if(currentlyPlaying >= 0){
         audio.setAttribute("src", `assets/${musicLibrary[currentlyPlaying].title.toLocaleLowerCase()}.mp3`);
-        if(audioStatusContainer === "paused"){
-            audio.play();
-        }
+        audioStatus.setAttribute("src", "assets/controls/pause.svg")
+        audio.play();
+        audioStatusContainer = "paused";
     }
 
     else{
